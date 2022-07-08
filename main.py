bff42c337a0c3fbf1a692b3086470a6d6164b18f
Mahsima Madanipour <mahsima.madanipour@gmail.com>
2022-07-08 18:28:42 +0430

    Merge parser and write abstact syntax tree on file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from parser import LLOneParser
 from tokenizer import Tokenizer
 
 if __name__ == '__main__':
@@ -7,12 +8,16 @@ if __name__ == '__main__':
 
     decaf_tokenizer = Tokenizer(decaf_code)
     tokens, symbols_table = decaf_tokenizer.run()
+    abstract_syntax_tree_string = LLOneParser("def int T_ID ( int T_ID , int T_ID ) { return T_ID + T_ID ; } def int T_ID ( ) { int T_ID ; T_ID = T_DEC ; return T_ID ( T_ID , T_DEC ) ; }")
 
     tokens_file = open("source_tokens.txt", "w")
     symbols_table_file = open("source_symbols_table.txt", "w")
+    abstract_syntax_tree_file = open("sample_code.txt", "w")
 
     tokens_string = "\n".join(["\t".join(list(map(str, tokens[i]))) for i in range(len(tokens))])
     tokens_file.write(tokens_string)
 
     symbols_table_string = "\n".join([str(i + 1) + "\t" + s for i, s in enumerate(symbols_table)])
     symbols_table_file.write(symbols_table_string)
+
+    abstract_syntax_tree_file.write(abstract_syntax_tree_string)
